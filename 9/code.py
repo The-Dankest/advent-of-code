9d624088dfdd197d9319a943d78fb358d027d072
Ryan Longacre <ryanlongacre@outlook.com>
2024-12-18 17:21:26 -0500

    add solution for 9 part 2

--- 9/code.py
+++ 9/code.py
@@ -3,26 +3,25 @@ line = input()
 disk = []
 count = 0
 for i in range(len(line)):
-    disk += ([-1 if i % 2 != 0 else count] * int(line[i]))
+    disk.append([-1 if i % 2 != 0 else count] * int(line[i]))
     count += i % 2
 
-reverse = [x for x in disk[::-1] if x != -1]
+i = len(disk) - 1
+while i > 0:
+    if len(disk[i]) and disk[i][0] > 0:
+        for j in range(i):
+            if len(disk[j]) >= len(disk[i]) and disk[j][0] == -1:
+                og = disk[i][::]
+                disk[j] = [-1] * (len(disk[j]) - len(disk[i]))
+                disk[i] = [-1] * len(disk[i])
+                disk.insert(j, og)
+                i += 1
+                break
+    i -= 1
 
-print(reverse)
-
-new = []
-count = 0
-for i in range(len(reverse)):
-    if disk[i] == -1:
-        new.append(reverse[count])
-        count += 1
-    else:
-        new.append(disk[i])
-
-
-# TODO: Checksum
 checksum = 0
-for i in range(len(new)):
-    checksum += (i * new[i])
+new_disk = [x for y in disk for x in y]
+for i in range(len(new_disk)):
+    checksum += (i * (max(new_disk[i], 0)))
 
 print(checksum)
